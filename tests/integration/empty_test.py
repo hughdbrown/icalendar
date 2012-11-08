5bc51e1658384679223a14e4b8487ef856adea78
Hugh Brown <hbrown@wgen.net>
2012-11-08 17:53:41 -0500

    Make real integration and unit tests

--- tests/integration/empty_test.py
+++ tests/integration/empty_test.py
@@ -1,18 +1,23 @@
 import unittest
 
-#from mock import Mock, patch
+# from mock import Mock, patch
 from nose.tools import (
-    raises,
-    assert_true, assert_false, assert_equals
+    # raises, assert_true, assert_false,
+    assert_equals
 )
 
+#from src import Calendar
+import translate_cal
 
-class TestSed(unittest.TestCase):
+
+class TestICalendar(unittest.TestCase):
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test_empty(self):
-        assert_true(True)
+    def test_create_events(self):
+        events = translate_cal.create_events('data/lori_cal.txt')
+        assert_equals(len(events), 28)
+        #ical = Calendar(events)
--- tests/unit/empty_test.py
+++ tests/unit/empty_test.py
@@ -1,18 +1,36 @@
 import unittest
+from datetime import datetime
 
-#from mock import Mock, patch
+# from mock import Mock, patch
 from nose.tools import (
-    raises,
-    assert_true, assert_false, assert_equals
+    # raises, assert_true, assert_false,
+    assert_equals
 )
 
+from src import Calendar, Event
 
-class TestSed(unittest.TestCase):
+
+class TestCalendar(unittest.TestCase):
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test_empty(self):
-        assert_true(True)
+    def test_equal_events(self):
+        tz = "America/New_York"
+        event1 = Event(
+            '20121011T000000Z',
+            '20121016T000000Z',
+            'Jim and Missy visit NYC'
+        )
+        cal1 = Calendar(tz, events=[event1])
+
+        event2 = Event(
+            datetime(2012, 10, 11, 0, 0, 0),
+            datetime(2012, 10, 16, 0, 0, 0),
+            'Jim and Missy visit NYC'
+        )
+        cal2 = Calendar(tz, events=[event2])
+
+        assert_equals(str(cal1), str(cal2))
